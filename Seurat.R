fc2f7c012a36f9c1475f0309e4972e1836600dfd
Charles Warden <cwarden45@gmail.com>
2025-02-23 10:56:43 -0800

    Add files via upload

--- Seurat.R
+++ Seurat.R
@@ -4,8 +4,18 @@ sampleID="LungCancer_FFPE"
 XeniumRanger_outs = "../Xenium_V1_humanLung_Cancer_FFPE_outs"
 
 library(Seurat)
+library(ggplot2)
 
 #follow example code : https://satijalab.org/seurat/articles/seurat5_spatial_vignette_2#mouse-brain-10x-genomics-xenium-in-situ
 #FOV = Field of View --> https://www.10xgenomics.com/support/software/xenium-onboard-analysis/latest/analysis/xoa-output-understanding-outputs
 
 xenium.obj = LoadXenium(XeniumRanger_outs, fov = "fov")
+print(ncol(xenium.obj))
+xenium.obj = subset(xenium.obj, subset = nCount_Xenium > 0)
+print(ncol(xenium.obj))
+
+VlnPlot(xenium.obj, features = c("nFeature_Xenium", "nCount_Xenium"), ncol = 2, pt.size = 0)
+ggsave(paste(sampleID,"-Seurat_import_nCount_gt_0-Feature_Violin.png",sep=""))
+
+ImageDimPlot(xenium.obj, fov = "fov", molecules = c("CDH16", "CENPF", "EGFR", "MKI67"), nmols = 20000)
+ggsave(paste(sampleID,"-Seurat_import_nCount_gt_0-ImagePlot_QC.png",sep=""))
